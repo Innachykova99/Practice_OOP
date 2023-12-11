afa43f6c6c1c6f004e8003541393c98e6bba1be2
inna.chykova@outlook.com <inna.chykova@outlook.com>
2023-12-11 15:21:45 +0200

    remove extra usings in Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Xml.Linq;
-using OOP_PracticeTask.Entities;
+﻿using OOP_PracticeTask.Entities;
 
 namespace OOP_PracticeTask
 {
